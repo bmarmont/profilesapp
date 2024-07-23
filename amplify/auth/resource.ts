616fd9c306c90021e97e80a73db1b53468d715b5
Ben Marmont <benmarmont@gmail.com>
2024-07-23 13:30:48 -0700

    fixing build issue

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -9,7 +9,7 @@ export const auth = defineAuth({
     email: {
       verificationEmailStyle: "CODE",
       verificationEmailSubject: "Welcome to the AI-Powered Recipe App!",
-      verificationEmailBody: (createCode) => "Use this code to confirm your account: ${createCode()}",
+      verificationEmailBody: (createCode) => `Use this code to confirm your account: ${createCode()}`,
     },
   },
 });
